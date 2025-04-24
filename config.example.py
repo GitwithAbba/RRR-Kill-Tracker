f22664de088a6a02a017295ef718374116cab644
GitWithAbba <mlz82591@gmail.com>
2025-04-24 02:39:07 -0400

    added config.example file

---
+++ config.example.py
@@ -0,0 +1,5 @@
+# config.example.py
+BACKEND_URL = "https://your-backend-url"
+API_KEY = "your-72h-api-key-here"
+VALIDATE_URL = f"{BACKEND_URL}/keys/validate"
+REPORT_KILL_URL = f"{BACKEND_URL}/reportKill"
